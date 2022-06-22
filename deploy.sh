251e9e64bcbe27e5841050003fe2dfca6ec5a401
ke.yang1@yintech.cn <2262658928@qq.com>
2022-06-22 22:00:57 +0800

    feat(config) 编辑部署脚本

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,3 @@ chmod +x ./gradlew
 ./gradlew clean assemble --refresh-dependencies -Denv=dev
 docker build -f src/main/docker/Dockerfile -t registry.cn-hangzhou.aliyuncs.com/yangke/yk-util:dev .
 docker push registry.cn-hangzhou.aliyuncs.com/yangke/yk-util:dev
-docker pull registry.cn-hangzhou.aliyuncs.com/yangke/yk-util:dev
